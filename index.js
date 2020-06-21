db5139f8f28d07afa8321cb8d0d6a2c43eb1bf61
azifcodes <61082527+azifcodes@users.noreply.github.com>
2020-06-22 00:08:48 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-function decrypt(message="", key="")
+function decrypt(message = "", key = "")
 {
-		var code = CryptoJS.AES.decrypt(message, key);
-		var decryptedMessage = code.toString(CryptoJS.enc.Utf8);
-		return decryptedMessage;
+    var code = CryptoJS.AES.decrypt(message, key);
+    var decryptedMessage = code.toString(CryptoJS.enc.Utf8);
+    return decryptedMessage;
 }
 
 function copy(btnElement, event)
 {
-		navigator.clipboard.writeText(btnElement.textContent);
+    navigator.clipboard.writeText(btnElement.textContent);
 }
 
 const tBody = document.getElementById("tBody");
@@ -16,26 +16,29 @@ const keyInp = document.getElementById("keyInp");
 const iCard = document.getElementById("iCard");
 const table = document.getElementById("table");
 
-btn.onclick = function()
+btn.onclick = function ()
 {
-		iCard.style = "display: none;";
-		
-		let key = keyInp.value;
-		
-		Papa.parse("https://azifcdn.imfast.io/data.csv",
-				{
-						download: true,
-						delimiter: function(input) {return "@$!"},
-						complete: function(res)
-						{
-								for (let i=0; i<res.data.length; i++)
-								{
-										let n = decrypt(res.data[i][1], key);
-										let d = decrypt(res.data[i][2], key);
-										tBody.innerHTML += "<tr><th scope='row'>"+ res.data[i][0] +"</th><td>" + n + "</td><td>" + '<button type="button" class="btn btn-light" onclick="copy(this, event)">' + d + '</button>' + "</td></tr>";
-								}
-								
-								table.style = "";
-						}
-				});
+    iCard.style = "display: none;";
+
+    let key = keyInp.value;
+
+    Papa.parse("https://azifcdn.imfast.io/data.csv",
+    {
+        download: true,
+        delimiter: function (input)
+        {
+            return "@$!"
+        },
+        complete: function (res)
+        {
+            for (let i = 0; i < res.data.length; i++)
+            {
+                let n = decrypt(res.data[i][1], key);
+                let d = decrypt(res.data[i][2], key);
+                tBody.innerHTML += "<tr><th scope='row'>" + res.data[i][0] + "</th><td>" + n + "</td><td>" + '<button type="button" class="btn btn-light" onclick="copy(this, event)">' + d + '</button>' + "</td></tr>";
+            }
+
+            table.style = "";
+        }
+    });
 }
